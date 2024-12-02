d912dd060b9adf6cc8aada721dbe999798204e18
ozwaldorf <self@ossian.dev>
2024-12-02 09:41:55 -0500

    chore(flake): cleanup and improvements

--- flake.nix
+++ flake.nix
@@ -4,40 +4,31 @@
     { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-
-        config.allowUnfree = true;
-        config.allowBroken = true;
-      };
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.default = (
-        pkgs.callPackage (
-          { lib, rustPlatform }:
-          rustPlatform.buildRustPackage {
-            name = "zoom-sync";
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-              addDriverRunpath
-            ];
-            buildInputs = with pkgs; [
-              systemd # for libudev
-              openssl # for http request to ipinfo and open-meteo
-            ];
-            fixupPhase = ''
-              addDriverRunpath $out/bin/zoom-sync
-            '';
-          }
-        ) { }
-      );
+      packages.${system}.default = pkgs.callPackage (
+        { lib, rustPlatform }:
+        rustPlatform.buildRustPackage {
+          name = "zoom-sync";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            addDriverRunpath
+          ];
+          buildInputs = with pkgs; [
+            systemd # for libudev
+            openssl # for http request to ipinfo and open-meteo
+          ];
+          # for exposing nvml dynamic library
+          fixupPhase = ''addDriverRunpath $out/bin/zoom-sync'';
+        }
+      ) { };
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          rustup
+          rustfmt
+          clippy
           rust-analyzer
         ];
         inputsFrom = [ self.packages.${system}.default ];
